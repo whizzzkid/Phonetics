515a744f9823b9736197695ee31e9a155c8ec60b
whizzzkid <me@nishantarora.in>
2020-03-30 02:06:16 -0600

    Making it work

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,46 @@
+import cleaner from 'rollup-plugin-cleaner';
 import copy from 'rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 const output = 'dist/extension';
+const defaultPlugins = [commonjs(), resolve()];
 
 export default [
+    {
+        input: 'src/pageAction.js',
+        output: {
+            file: `${output}/pa.js`,
+            format: 'iife',
+        },
+        plugins: [
+            ...defaultPlugins,
+            cleaner({
+                targets: ['./dist/'],
+            }),
+            copy({
+                targets: [{
+                    src: ['node_modules/hint.min.css', 'src/manifest.json', 'src/assets'],
+                    dest: `${output}/`,
+                }],
+                flatten: false,
+            }),
+        ],
+    },
     {
         input: 'src/contentScript.js',
         output: {
             file: `${output}/cs.js`,
-            format: 'umd',
+            format: 'iife',
         },
-        plugins: [
-            copy({
-                targets: [
-                    { src: 'node_modules/hint.min.css', dest: `${output}/hint.css` },
-                ],
-            }),
-        ],
+        plugins: [ ...defaultPlugins ],
     },
     {
         input: 'src/background.js',
         output: {
             file: `${output}/bg.js`,
-            format: 'umd',
+            format: 'iife',
         },
-    },
-    {
-        input: 'src/pageAction.js',
-        output: {
-            file: `${output}/pa.js`,
-            format: 'umd',
-        },
-        plugins: [
-            copy({
-                targets: [
-                    { src: 'src/manifest.json', dest: `${output}/manifest.json` },
-                    { src: 'src/assets/*', dest: `${output}/assets` },
-                ],
-                flatten: false,
-            }),
-        ],
+        plugins: [ ...defaultPlugins ],
     },
 ];
